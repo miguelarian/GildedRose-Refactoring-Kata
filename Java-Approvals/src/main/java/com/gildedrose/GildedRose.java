a4eb906a679b9a5c77ab31dd2c124547eb3709ab
miguelarian <miguelvr88@gmail.com>
2024-07-31 13:03:35 +0200

    add decrement parameter to decreaseQuality method

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -16,7 +16,7 @@ class GildedRose {
             Item item = items[i];
 
             if (isStandardItem(item)) {
-                decreaseQuality(item);
+                decreaseQuality(item, 1);
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
@@ -45,7 +45,7 @@ class GildedRose {
                 if (!isAgedBrie(item)) {
                     if (!isBackstagePass(item)) {
                         if (!isSulfuras(item)) {
-                            decreaseQuality(item);
+                            decreaseQuality(item, 1);
                         }
                     } else {
                         item.quality = item.quality - item.quality;
@@ -59,9 +59,9 @@ class GildedRose {
         }
     }
 
-    private static void decreaseQuality(Item item) {
+    private static void decreaseQuality(Item item, int decrement) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            item.quality = item.quality - decrement;
         }
     }
 
